8bf787a84175275b7cb0bb68099f340b07f9ba4c
liucb <liucb>
2020-11-13 10:37:40 +0800

    分享功能

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-import { View, Image } from '@tarojs/components'
-import './index.less'
-import data from '../../data/picData'
+import React, { Component } from "react";
+import { View, Image } from "@tarojs/components";
+import "./index.less";
+import data from "../../data/picData";
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  onShareAppMessage() {
+    return {
+      title: "今日美女",
+    };
+  }
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        {
-          data.map(i => {
-            return <Image mode="widthFix" className="girl-image" src={i}></Image>
-          })
-        }
+      <View className="index">
+        {data.map((i) => {
+          return <Image mode="widthFix" className="girl-image" src={i}></Image>;
+        })}
       </View>
-    )
+    );
   }
 }
